b6af9d9a8648612fc43bdca7d0a72beb14b1b7cb
7uuuuu <heibaiyin7@gmail.com>
2024-08-16 05:00:37 +0800

    建立斜線指令 ping

---
+++ src/commands/ping/index.js
@@ -0,0 +1,7 @@
+import { SlashCommandBuilder } from "discord.js"
+
+export const command = new SlashCommandBuilder().setName("ping").setDescription("ping command")
+
+export const action = async (ctx) => {
+    ctx.reply("Pong !")
+}
